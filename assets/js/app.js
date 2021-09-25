8b943172b22ed733241026eddfc914fb3fe0bb07
NordDev <sver4ok@protonmail.com>
2021-09-25 13:00:45 +0300

    AT/add basic-frontend

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+document.querySelector('.puzzle').addEventListener('click', function (event) {
+    let element = Object.assign(event.target);
+    let position = element.getAttribute('data-value').split(',');
+
+    let row = +position[0], column = +position[1];
+
+    if (!document.getElementById("cell"+row+column).classList.contains("tile9")) {
+        if (column < 3) {
+            if (document.getElementById("cell"+row+(column+1)).classList.contains("tile9")) {
+                swapTiles("cell"+row+column,"cell"+row+(column+1));
+                return;
+            }
+        }
+        if (column > 1) {
+            if (document.getElementById("cell"+row+(column-1)).classList.contains("tile9")) {
+                swapTiles("cell"+row+column,"cell"+row+(column-1));
+                return;
+            }
+        }
+        if (row > 1) {
+            if (document.getElementById("cell"+(row-1)+column).classList.contains("tile9")) {
+                swapTiles("cell"+row+column,"cell"+(row-1)+column);
+                return;
+            }
+        }
+        if (row < 3) {
+            if (document.getElementById("cell"+(row+1)+column).classList.contains("tile9")) {
+                swapTiles("cell"+row+column,"cell"+(row+1)+column);
+                return;
+            }
+        }
+    }
+});
+
+function swapTiles(cell1, cell2) {
+    let temp = document.getElementById(cell1).className;
+    document.getElementById(cell1).className = document.getElementById(cell2).className;
+    document.getElementById(cell2).className = temp;
+}
--- index.php
+++ index.php
@@ -2,9 +2,41 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-use App\Classes\Puzzle;
+//use App\Classes\Puzzle;
+//
+//$puzzle = new Puzzle();
+//$solution = $puzzle->run($puzzle->createRandomPuzzle());
+//
+//echo $solution;
+?>
 
-$puzzle = new Puzzle();
-$solution = $puzzle->run($puzzle->createRandomPuzzle());
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>8-Puzzle</title>
 
-echo $solution;
+        <link rel="stylesheet" href="assets/css/app.css">
+    </head>
+    <body>
+        <div class="puzzle">
+            <div class="row">
+                <div id="cell11" class="col tile1" data-value="1,1"></div>
+                <div id="cell12" class="col tile2" data-value="1,2"></div>
+                <div id="cell13" class="col tile3" data-value="1,3"></div>
+            </div>
+            <div class="row">
+                <div id="cell21" class="col tile4" data-value="2,1"></div>
+                <div id="cell22" class="col tile5" data-value="2,2"></div>
+                <div id="cell23" class="col tile6" data-value="2,3"></div>
+            </div>
+            <div class="row">
+                <div id="cell31" class="col tile7" data-value="3,1"></div>
+                <div id="cell32" class="col tile8" data-value="3,2"></div>
+                <div id="cell33" class="col tile9" data-value="3,3"></div>
+            </div>
+        </div>
+
+        <script src="assets/js/app.js"></script>
+    </body>
+</html>
